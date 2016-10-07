237a5bf5c6a1d876edc3e185aca3a760dd4a94ac
Jan Pogocki <jan.pogocki@gmail.com>
2016-10-07 02:11:38 +0200

    front-end

---
+++ src/splitpdf/Controller.java
@@ -0,0 +1,4 @@
+package splitpdf;
+
+public class Controller {
+}
---
+++ src/splitpdf/Main.java
@@ -0,0 +1,24 @@
+package splitpdf;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
+        primaryStage.setTitle("SplitPDF - podziel strony!");
+        primaryStage.setScene(new Scene(root, 500, 210));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
